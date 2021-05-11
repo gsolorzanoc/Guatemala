9874d9ed7a6f755a2ff541f0c9ae60493d38da76
Lyesgamove <Lyndamorales11@gmail.com>
2021-05-11 15:38:41 -0600

    Cambio de Lynda

---
+++ Turismo-Lynda.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Informacion Relacionada</title>
+	<meta charset="utf-8">
+</head>
+<body background="fondo1.jpg">
+
+<center>
+<b><font face="Monotype Corsiva" size="8">Conozca el Lago de Amatitlán</font></b><br><br>
+
+	<font face="Arial" size="4">
+		El lago de Amatitlán es un lago de cráter situado en Guatemala. Se encuentra a 26 km de la Ciudad de <br>
+		Guatemala, a una altitud de 1188 msnm. Tiene 12 km de largo, 3 km de ancho, y una superficie de 15.2 km²<br> 
+		y es el cuarto cuerpo de agua más grande de Guatemala. Su profundidad máxima es de 33 m y su profundidad<br> 
+		media es 18 m. El volumen del cuerpo de agua es aproximadamente 0.286 km³. Administrativamente el lago se<br> 
+		encuentra dentro del Departamento de Guatemala, no obstante en el interior del lago se encuentra la division <br>administrativa de Villa Nueva, Villa Canales, San Miguel Petapa y Amatitlán por lo que cada uno de esos<br>
+		municipios tienen parte del lago dentro de su jurisdicción, además la cuenca del lago esta formada por 14 <br>municipios, 7 de ellos influyen directamente en el lago debido a la degradación de los recursos naturales, <br>
+		siendo estos: Ciudad de Villa Nueva, Villa Canales, Santa Catarina Pinula, San Miguel Petapa, Ciudad de <br>
+		Mixco, Amatitlán y las zonas 11, 12 y 21 de la Ciudad de Guatemala. La población de la cuenca es de más<br> 
+		de 2 millones de habitantes; siendo Villa Nueva el municipio más poblado<br><br>
+		<img src="lagp.jpg" height="300" width="400"><br><br>
+
+		<b>Origen</b><br>
+		El lago de Amatitlán tiene su origen en la Era Cuaternaria y su formación se debe a los movimientos <br>
+		tecto-volcánicos ocurridos en el área y provocados por los volcanes de Pacaya, Agua, Fuego y Acatenango.<br> 
+		Durante esa época, su extensión era de aproximadamente 80km2 , ocupando la superficie de los municipios de<br>
+		Amatitlán, Villa Nueva y Villa Canales ya que se han encontrado varios fósiles de caracoles y pequeños<br>
+		esqueletos de peces durante excavaciones realizadas en esos lugares a principios de siglo. Durante la época<br>
+	 	precolombina, el lago fue la fuente principal de abastecimiento de agua y de alimento para las tribus Pocomam<br> asentadas a su alrededor. <br><br>
+
+		<b>Contaminación del Lago</b><br>
+		El río Villalobos que alimenta el lago, es uno de los principales desagües de aguas servidas domésticas, <br>industriales y agroindustriales del área metropolitana de la Ciudad de Guatemala. Cada año un volumen <br>
+		importante de aguas negras y 500,000 metros cúbicos de sedimentos entran en el lago.5​6​ Esta situación ha <br>
+		resultado en altos niveles de contaminación y una acelerada eutrofización y sedimentación, afectando las<br> 
+		funciones del lago como fuente de agua potable, para la pesca y el riego, y ha reducido sus funciones<br> 
+		recreativas.<br><br>
+		<img src="mapa2.png" height="300" width="400"><br><br>
+
+
+	</font>
+</center>
+</body>
+</html>
