b5e7d31379c0f9cc62012e3f6443cb47f40a0b71
elmermendez1 <elmer1mendez2@gmail.com>
2021-05-11 12:33:23 -0600

    Elmer

---
+++ turismo_Elmer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lugares Turisticos</title>
+</head>
+<body>
+	<?php
+require "encabezado.php";
+	?>
+<div id="fondo">
+	<center><font face="ravie" color="#0B615E" size="32">Lugares Turisticos de Izabal</font></center><br><br><br><br><br>
+	<center><table><tr><td><div class="card" style="width: 18rem;">
+  <img src="i2.JPG" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">El castillo de San Felipe de Lara es una fortaleza ubicada en el municipio de Livingston en el oriente de Guatemala.<br><br></p>
+  </div>
+</div></td>
+<td><div class="card" style="width: 18rem;">
+  <img src="i3.JPG" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">El lago de Izabal, también conocido como golfo Dulce, es un lago que se encuentra en el departamento de Izabal, cerca de la costa del Mar Caribe de Guatemala.</p>
+  </div>
+</div></td><td>
+<div class="card" style="width: 18rem;">
+  <img src="i4.JPG" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">El río Dulce se encuentra en el departamento de Izabal, Guatemala, entre el lago de Izabal y la bahía de Amatique de alrededor de 43 km de largo.</p>
+  </div>
+</div></td></tr></table></center><br><br><br><br><br><br><br><br><br>
+</div>
+</body>
+</html>
